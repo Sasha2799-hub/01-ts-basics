db18f7435d0c73e783d948f642706c551c9904cd
Sasha2799-hub <alex0kuz99@gmail.com>
2025-10-26 21:00:19 +0200

    correct task8

--- src/task-8.ts
+++ src/task-8.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export function task8() {
-  async function fetchPosts() {
+  async function fetchPosts():Promise<Post[]>{
     const response = await axios.get<Post[]>(
       "https://jsonplaceholder.typicode.com/posts"
     );
